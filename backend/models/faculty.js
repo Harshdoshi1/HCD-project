d4e80fa696ba95a42cb6c3caeb5b237d23685db0
Rishit Rathod <rishitrathod52@gmail.com>
2025-02-07 22:44:52 +0530

    no need

--- backend/models/faculty.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const facultySchema = new mongoose.Schema({
-    _id: { type: mongoose.Schema.Types.ObjectId, auto: true }, // Faculty ID (PK)
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true }, // Foreign Key
-}, { timestamps: true });
-
-module.exports = mongoose.model('Faculty', facultySchema);
