3f996aa63d5b94295cc880794561da51ed7ed619
Rishit Rathod <rishitrathod52@gmail.com>
2025-02-09 11:18:53 +0530

    semester add batch wise and assign faculty modified not completed

--- backend/models/semester.js
+++ backend/models/semester.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const semesterSchema = new mongoose.Schema({
     batchId: { type: mongoose.Schema.Types.ObjectId, ref: "Batch", required: true },
     semesterNumber: { type: Number, required: true, min: 1, max: 8 },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true }
 });
 
 module.exports = mongoose.model("Semester", semesterSchema);
