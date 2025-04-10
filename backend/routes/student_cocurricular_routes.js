2cf942b37c81947f71fb3c694014b70402af417e
krish mamtora <krishmamtora26@gmail.com>
2025-04-10 17:42:15 +0530

    student points table structure rearranged and created new table for event participation types

--- backend/routes/student_cocurricular_routes.js
+++ backend/routes/student_cocurricular_routes.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const router = express.Router();
-const {
-    addActivity,
-    updateActivity,
-    deleteActivity,
-    getStudentActivities,
-    getStudentActivitieswithenrollmentandSemester
+// const express = require('express');
+// const router = express.Router();
+// const {
+//     addActivity,
+//     updateActivity,
+//     deleteActivity,
+//     getStudentActivities,
+//     getStudentActivitieswithenrollmentandSemester
 
-} = require('../controller/studentCocurricularController');
+// } = require('../controller/studentCocurricularController');
 
-// Add new co-curricular activity
-router.post('/', addActivity);
+// // Add new co-curricular activity
+// router.post('/', addActivity);
 
-// Update existing co-curricular activity
-router.put('/:activityId', updateActivity);
+// // Update existing co-curricular activity
+// router.put('/:activityId', updateActivity);
 
-// Delete co-curricular activity
-router.delete('/:activityId', deleteActivity);
+// // Delete co-curricular activity
+// router.delete('/:activityId', deleteActivity);
 
-// Get activities for a student by enrollment number
-router.post('/getco', getStudentActivities);//update route
+// // Get activities for a student by enrollment number
+// router.post('/getco', getStudentActivities);//update route
 
-router.post('/getCoWithSem', getStudentActivitieswithenrollmentandSemester);
+// router.post('/getCoWithSem', getStudentActivitieswithenrollmentandSemester);
 
-module.exports = router;
+// module.exports = router;
