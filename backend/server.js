987190909c15c29898cf1bd0f0585ba66faeeb9e
krish mamtora <krishmamtora26@gmail.com>
2025-05-22 02:12:09 +0530

    send email working

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const studentCPIRoutes = require('./routes/studentCPI_routes');
 const gradesRoutes = require('./routes/grades_routes');
 const academicDetailsRoutes = require('./routes/academic_details_routes');
 const app = express();
-
+const emailRoutes = require('./routes/email_routes');
 // Enable CORS
 app.use(cors({
     // origin: 'http://localhost:5173',
@@ -31,8 +31,15 @@ app.use(cors({
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Increase payload size limit for file uploads
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Routes
+app.use('/api/email', emailRoutes);
+
 app.use('/api/users', userRoutes);
 app.use('/api/batches', batchRoutes);
 app.use('/api/faculties', facultyRoutes);
